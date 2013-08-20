55cd199e61774c67342f75ff45d311f79b2b1776
Robert Court <robbie.court@gmail.com>
2013-08-20 13:08:36 +0100

    modified tif2wlzDom to accept other variables

--- tif2wlzDom.py
+++ tif2wlzDom.py
@@ -1,15 +1,30 @@
 import glob, subprocess
 
-wlzdir = '/disk/data/VFBTools/Woolz2013Full/bin/'
-Tfile = 'flyVNCtemplate20xDaS_th.wlz'
+if (len(sys.argv) < 4):
+    print 'e.g. python tif2wlzDom.py DomainPrefix templateFile WoolzDir FijiExec'
+    print 'Error: missing arguments - using defaults:' 
+    print 'e.g. python tif2wlzDom.py TAGdomain flyVNCtemplate20xDaS_th.wlz /disk/data/VFBTools/Woolz2013Full/bin/ /disk/data/VFBTools/Fiji.145/fiji-linux64'
+    wlzdir = '/disk/data/VFBTools/Woolz2013Full/bin/'
+    Tfile = 'flyVNCtemplate20xDaS_th.wlz'
+    Fpre = 'TAGdomain'
+    Lfiji = '/disk/data/VFBTools/Fiji.145/fiji-linux64'
+else:
+    Lfiji = str(sys.argv[4])
+    wlzdir = str(sys.argv[3])
+    Tfile = str(sys.argv[2])
+    Fpre = str(sys.argv[1])
+    
+for infile in glob.glob(Fpre + "0*.nrrd"):
+    print 'Converting %s to %s...' % (infile, infile.replace('.nrrd','.tif')
+    subprocess.call('%s -macro nrrd2tif.ijm %s -batch' % (infile), shell=True) 
 
 for i in xrange(1,250):
-    outfile = 'TAGdomain' + str(i).zfill(4) + '_dom.wlz'
+    outfile = Fpre + str(i).zfill(4) + '_dom.wlz'
     print 'Creating blank %s...'% outfile
     subprocess.call('%sWlzMakeEmpty >%s'% (wlzdir, outfile), shell=True)
     
 
-for infile in glob.glob("TAGdomain0*.tif"):
+for infile in glob.glob(Fpre + "0*.tif"):
     outfile = infile.replace('.tif','_dom.wlz')
     print 'Converting %s to %s...' % (infile, outfile)
     subprocess.call('%sWlzExtFFConvert -ftif -Fwlz %s | %sWlzThreshold -v1 |%sWlzDomain >%s' % (wlzdir, infile, wlzdir, wlzdir, outfile), shell=True)
@@ -22,6 +37,6 @@ for infile in glob.glob("TAGdomain0*.tif"):
 
 print 'Compiling...'
 
-subprocess.call('cat %s TAGdomain0*_dom.wlz | %sWlzCompound >out.wlz'% (Tfile, wlzdir), shell=True)
+subprocess.call('cat %s ' + Fpre + '0*_dom.wlz | %sWlzCompound >out.wlz'% (Tfile, wlzdir), shell=True)
 
 print 'Result saved to out.wlz'
