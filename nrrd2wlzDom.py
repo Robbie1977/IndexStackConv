9900a080fa6713c8d98b687db0fa1d2b545b2641
Robert Court <robbie.court@gmail.com>
2013-08-27 17:23:52 +0100

    Changes to fix voxel size - new variable req!

--- nrrd2wlzDom.py
+++ nrrd2wlzDom.py
@@ -13,6 +13,7 @@ else:
     wlzdir = str(sys.argv[3])
     Tfile = str(sys.argv[2])
     Fpre = str(sys.argv[1])
+    Zsize = str(sys.argv[5])
     
 for infile in glob.glob(Fpre + "0*.nrrd"):
     print 'Converting %s to %s...' % (infile, infile.replace('.nrrd','.tif'))
@@ -27,7 +28,7 @@ for i in xrange(1,250):
 for infile in glob.glob(Fpre + "0*.tif"):
     outfile = infile.replace('.tif','_dom.wlz')
     print 'Converting %s to %s...' % (infile, outfile)
-    subprocess.call('%sWlzExtFFConvert -ftif -Fwlz %s | %sWlzThreshold -v1 |%sWlzDomain |%sWlzSetVoxelSize -x1 -y1 -z1.5 >%s' % (wlzdir, infile, wlzdir, wlzdir, wlzdir, outfile), shell=True)
+    subprocess.call('%sWlzExtFFConvert -ftif -Fwlz %s | %sWlzThreshold -v1 |%sWlzDomain |%sWlzSetVoxelSize -x1 -y1 -z%s >%s' % (wlzdir, infile, wlzdir, wlzdir, wlzdir, Zsize, outfile), shell=True)
     comfile = infile.replace('.tif','.txt')
     print 'Calculating centre for %s and saving to %s...' % (outfile, comfile)
     subprocess.call('%sWlzCentreOfMass -o %s %s' % (wlzdir, comfile, outfile), shell=True)
@@ -37,10 +38,10 @@ for infile in glob.glob(Fpre + "0*.tif"):
 
 print 'Compiling...'
 
+subprocess.call('%sWlzSetVoxelSize -x1 -y1 -z%s %s >%s_E' % (wlzdir, Zsize, Tfile, Tfile), shell=True)
+
 subprocess.call('find %s0*_dom.wlz -size -2b | xargs rm -f'% (Fpre), shell=True)
 
-subprocess.call('%sWlzSetVoxelSize -x1 -y1 -z1.5 %s >%s' % (wlzdir, Tfile, Tfile), shell=True)
-
-subprocess.call('cat %s %s0*_dom.wlz | %sWlzCompound >out.wlz'% (Tfile, Fpre, wlzdir), shell=True)
+subprocess.call('cat %s_E %s0*_dom.wlz | %sWlzCompound >out.wlz'% (Tfile, Fpre, wlzdir), shell=True)
 
 print 'Result saved to out.wlz'
