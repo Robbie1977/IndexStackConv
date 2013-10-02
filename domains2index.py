047c48da51f423b1fcdb3f2c39a03e6719175d90
Robert Court <robbie.court@gmail.com>
2013-10-02 21:24:35 +0100

    Added domain to index file conversion

---
+++ domains2index.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os
+import sys
+import nrrd
+
+if (len(sys.argv) < 3):
+    print 'Error: missing arguments!' 
+    print 'e.g. python domain2index.py indexfile.nrrd domainfile1.nrrd domainfileN.nrrd ...'
+else:
+     
+    print 'Adding to index', str(sys.argv[1]), '....'
+    for x in range(2,(len(sys.argv))):
+        print 'adding data from file', sys.argv[x]
+        readdata, options = nrrd.read(str(sys.argv[x]))
+        for i in range(1,255):
+            if np.uint8(i) in readdata: 
+                print 'appending index', str(i)
+                indfile = str(sys.argv[1])
+                if os.path.exists(indfile):
+                    domain, options = nrrd.read(indfile)
+                else:
+                    domain = np.zeros(readdata.shape,np.uint8)
+                domain[readdata==i]=np.uint8(i)
+                nrrd.write(indfile, domain)
+    
+    
+    
+    
+print 'Done.'
