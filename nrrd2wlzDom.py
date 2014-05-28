25f351adf455992507714bd9327dea380571863c
Robert Court <robbie.court@gmail.com>
2014-05-28 01:21:44 +0100

    offer default settings

--- nrrd2wlzDom.py
+++ nrrd2wlzDom.py
@@ -1,8 +1,41 @@
 import glob, subprocess, sys
 
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"yes":False,   "y":False,  "ye":False,
+             "no":True,     "n":True}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "\
+                             "(or 'y' or 'n').\n")
+
+
 if (len(sys.argv) < 4):
     print 'e.g. python nrrd2wlzDom.py DomainPrefix templateFile WoolzDir FijiExec Zsize XYsize'
-    print 'Error: missing arguments - using defaults:' 
+    print 'Error: missing arguments - using defaults:'
     print 'e.g. python nrrd2wlzDom.py TAGdomain flyVNCtemplate20xDaS_th.wlz /disk/data/VFBTools/Woolz2013Full/bin/ /disk/data/VFBTools/Fiji.145/fiji-linux64'
     wlzdir = '/disk/data/VFBTools/Woolz2013Full/bin/'
     Tfile = 'flyVNCtemplate20xDaS_th.wlz'
@@ -10,6 +43,7 @@ if (len(sys.argv) < 4):
     Lfiji = '/disk/data/VFBTools/Fiji.145/fiji-linux64'
     Zsize = str(1)
     Vsize = str(1)
+    if query_yes_no('Do you want to run with the defaults?', default='no'): break
 else:
     Lfiji = str(sys.argv[4])
     wlzdir = str(sys.argv[3])
@@ -17,16 +51,16 @@ else:
     Fpre = str(sys.argv[1])
     Zsize = str(sys.argv[5])
     Vsize = str(sys.argv[6])
-    
+
 for infile in glob.glob(Fpre + "0*.nrrd"):
     print 'Converting %s to %s...' % (infile, infile.replace('.nrrd','.tif'))
-    subprocess.call('nice xvfb-run %s -macro nrrd2tif.ijm %s -batch' % (Lfiji, infile), shell=True) 
+    subprocess.call('nice xvfb-run %s -macro nrrd2tif.ijm %s -batch' % (Lfiji, infile), shell=True)
 
 for i in xrange(1,250):
     outfile = Fpre + str(i).zfill(4) + '_dom.wlz'
     print 'Creating blank %s...'% outfile
     subprocess.call('nice %sWlzMakeEmpty -o%s'% (wlzdir, outfile), shell=True)
-    
+
 
 for infile in glob.glob(Fpre + "0*.tif"):
     outfile = infile.replace('.tif','_dom.wlz')
@@ -35,7 +69,7 @@ for infile in glob.glob(Fpre + "0*.tif"):
     comfile = infile.replace('.tif','.txt')
     print 'Calculating centre for %s and saving to %s...' % (outfile, comfile)
     subprocess.call('nice %sWlzCentreOfMass -o %s %s' % (wlzdir, comfile, outfile), shell=True)
-    
+
     #print '%sWlzExtFFConvert -ftif -Fwlz %s | %sWlzDomain >%s' % (wlzdir, infile, wlzdir, outfile)
 
 
@@ -43,7 +77,7 @@ print 'Compiling...'
 
 subprocess.call('%sWlzSetVoxelSize -x%s -y%s -z%s %s >%s_E' % (wlzdir, Vsize, Vsize, Zsize, Tfile, Tfile), shell=True)
 
-#subprocess.call('find %s0*_dom.wlz -size -2b | xargs rm -f'% (Fpre), shell=True) 
+#subprocess.call('find %s0*_dom.wlz -size -2b | xargs rm -f'% (Fpre), shell=True)
 
 subprocess.call('cat %s_E %s0*_dom.wlz | %sWlzCompound >out.wlz'% (Tfile, Fpre, wlzdir), shell=True)
 
